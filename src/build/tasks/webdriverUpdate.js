adb70c3da4ed50f454111162c615854cd527f81c
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2017-06-11 17:02:30 +1000

    fix webdriver manager update

--- src/build/tasks/webdriverUpdate.js
+++ src/build/tasks/webdriverUpdate.js
@@ -1,6 +1,26 @@
-const gulpProtractor = require('gulp-protractor')
+const path = require('path')
+const childProcess = require('child_process')
 
-// TODO - need to detect which browser drivers are required - probably in protractor conf
+var winExt = /^win/.test(process.platform) ? '.cmd' : '';
+
+function getProtractorDir() {
+  const result = require.resolve('protractor');
+  if (result) {
+    return path.resolve(path.join(path.dirname(result), '..', 'bin'))
+  }
+  throw new Error('No protractor installation found.');
+}
+
+function webdriverUpdate (opts, cb) {
+  const callback = (cb ? cb : opts);
+  const options = (cb ? opts : null);
+  const args = ['update', '--standalone', '--no-gecko'];
+
+  childProcess.spawn(path.resolve(getProtractorDir() + '/webdriver-manager' + winExt), args, {
+    stdio: 'inherit'
+  }).once('close', callback);
+};
+
 module.exports = function (gulp) {
-  return gulpProtractor.webdriver_update
+  return webdriverUpdate
 }
