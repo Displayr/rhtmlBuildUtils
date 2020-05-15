0a1ea0ee8d45f2a86e4d717e3b0a764fd18214ae
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-05-15 14:02:44 +1000

    allow missing crossExperimentSnapshotComparisons.yaml file

--- src/tasks/experiment/moveCrossExperimentSnapshotComparisonListToBrowser/index.js
+++ src/tasks/experiment/moveCrossExperimentSnapshotComparisonListToBrowser/index.js
@@ -4,7 +4,7 @@ const { basePath, experimentDirectory } = require('../../../lib/widgetConfig')
 
 module.exports = function (gulp) {
   return function (done) {
-    gulp.src([path.join(basePath, experimentDirectory, 'crossExperimentSnapshotComparisons.yaml')], {})
+    gulp.src([path.join(basePath, experimentDirectory, 'crossExperimentSnapshotComparisons.yaml')], { allowEmpty: true })
       .pipe(gulp.dest('browser/content'))
       .on('finish', done)
   }
