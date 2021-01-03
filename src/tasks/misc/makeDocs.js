761268fb10ebfa33ff7ef5b1ebe88f29e39669a6
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2021-01-03 12:20:47 +1000

    echo error on makeDocs fail

--- src/tasks/misc/makeDocs.js
+++ src/tasks/misc/makeDocs.js
@@ -2,8 +2,9 @@ const shell = require('shelljs')
 
 module.exports = function (gulp) {
   return function (done) {
-    const exitCode = shell.exec('r --no-save 2>/dev/null >/dev/null <<< "library(devtools); document()"').code
-    const error = (exitCode === 0) ? null : new Error(`make docs failed with code ${exitCode}`)
-    done(error)
+    const commandString = 'r --no-save 2>/dev/null >/dev/null <<< "library(devtools); document()"'
+    const exitCode = shell.exec(commandString).code
+    const callbackResponse = (exitCode === 0) ? null : new Error(`make docs failed with code ${exitCode}. Command was '${commandString}'`)
+    done(callbackResponse)
   }
 }
